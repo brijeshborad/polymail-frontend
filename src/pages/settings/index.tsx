16969accfeeea9e8aa4966aed154aa94e0d00d31
Brijesh Borad <brijeshborad29@gmail.com>
2023-08-23 14:01:17 +0530

    build issue fix

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React, {useCallback, useEffect} from "react";
 import withAuth from "@/components/withAuth";
 import {
     Grid,
