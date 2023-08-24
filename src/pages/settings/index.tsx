4a17f11d4119f4d413b2a6b4e24bee9ab6f18813
Brijesh Borad <brijeshborad29@gmail.com>
2023-08-24 21:33:18 +0530

    Update menu for setting paeg

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,86 +1,75 @@
-import React, {useCallback, useEffect} from "react";
-import {
-    Heading,
-    Flex,
-    UnorderedList,
-    ListItem,
-} from "@chakra-ui/react";
+import React, {useCallback} from "react";
+import {Flex, Heading, ListItem, UnorderedList,} from "@chakra-ui/react";
 import styles from "@/styles/setting.module.css";
 import {UserIcon} from "@/icons";
 import Router, {useRouter} from "next/router";
 
-let activeTab = ''
+const tabMenu = [
+    {
+        title: 'My Account',
+        children: [
+            {
+                title: 'Profile',
+                route: '/settings/profile'
+            },
+            {
+                title: 'Signature',
+                route: '/settings/signature'
+            },
+            {
+                title: 'Email Address',
+                route: '/settings/email-address'
+            },
+            {
+                title: 'Billing',
+                route: '/settings/billing'
+            },
+
+        ]
+    },
+    {
+        title: 'Workspace',
+        children: [
+            {
+                title: 'Members',
+                route: '/settings/members'
+            }
+
+        ]
+    }
+];
 
 function Index() {
     const router = useRouter()
 
-    let currentRoute = router.pathname.split('/');
-    const openTabs = useCallback((type: string) => {
-        if (type === 'profile') {
-            activeTab = 'Profile'
-            Router.push('/settings/profile', undefined, {shallow: true});
-        } else if (type === 'signature') {
-            activeTab = 'Signature'
-            Router.push('/settings/signature', undefined, {shallow: true});
-        } else if (type === 'email_address') {
-            activeTab = 'Email Address'
-            Router.push('/settings/email-address', undefined, {shallow: true});
-        } else if (type === 'billing') {
-            activeTab = 'Billing'
-            Router.push('/settings/billing', undefined, {shallow: true});
-        } else if (type === 'members') {
-            Router.push('/settings/members', undefined, {shallow: true});
-            activeTab = 'Members'
-        }
-    }, []);
-
-    useEffect(() => {
-        if (currentRoute[currentRoute.length - 1] === 'profile') {
-            activeTab = 'Profile'
-        } else if (currentRoute[currentRoute.length - 1] === 'signature') {
-            activeTab = 'Signature'
-        } else if (currentRoute[currentRoute.length - 1] === 'email-address') {
-            activeTab = 'Email Address'
-        } else if (currentRoute[currentRoute.length - 1] === 'billing') {
-            activeTab = 'Billing'
-        } else if (currentRoute[currentRoute.length - 1] === 'members') {
-            activeTab = 'Members'
-        }
-    }, [currentRoute])
+    let currentRoute = router.pathname;
+    const openTabs = useCallback((menuItem: {route: string, title:string}) => {
+        currentRoute = menuItem.route;
+        Router.push(menuItem.route);
+    }, [])
 
 
     return (
         <>
             <Heading as='h4' mb={8} className={styles.settingTitle}> Settings </Heading>
 
-            <Flex direction={'column'} mb={8}>
-                <Heading display={'flex'} alignItems={'center'} mb={2} as='h5' size='sm'
-                         className={styles.settingListTitle} textTransform={'uppercase'}><UserIcon/> My
-                    Account</Heading>
+            {tabMenu.map((tab, index: number) => (
+                <Flex direction={'column'} mb={8} key={index + 1}>
+                    <Heading display={'flex'} alignItems={'center'} mb={2} as='h5' size='sm'
+                             className={styles.settingListTitle} textTransform={'uppercase'}>
+                        <UserIcon/>{tab.title}
+                    </Heading>
+                    {tab.children &&
+                        <UnorderedList display={'flex'} gap={1} className={styles.settingList}>
+                            {tab.children.map((item, i: number) => (
+                                <ListItem key={i+1} onClick={() => openTabs(item)}
+                                          className={currentRoute === item.route ? styles.active : ''}>{item.title}</ListItem>
+                            ))}
+                        </UnorderedList>
+                    }
+                </Flex>
+            ))}
 
-                <UnorderedList display={'flex'} gap={1} className={styles.settingList}>
-                    <ListItem onClick={() => openTabs('profile')}
-                              className={activeTab === 'Profile' ? styles.active : ''}>Profile</ListItem>
-                    <ListItem onClick={() => openTabs('signature')}
-                              className={activeTab === 'Signature' ? styles.active : ''}>Signature</ListItem>
-                    <ListItem onClick={() => openTabs('email_address')}
-                              className={activeTab === 'Email Address' ? styles.active : ''}>Email
-                        Addresses</ListItem>
-                    <ListItem onClick={() => openTabs('billing')}
-                              className={activeTab === 'Billing' ? styles.active : ''}>Billing</ListItem>
-                </UnorderedList>
-            </Flex>
-
-            <Flex direction={'column'}>
-                <Heading display={'flex'} alignItems={'center'} mb={2} as='h5' size='sm'
-                         className={styles.settingListTitle}
-                         textTransform={'uppercase'}><UserIcon/> Workspace</Heading>
-
-                <UnorderedList className={styles.settingList}>
-                    <ListItem onClick={() => openTabs('members')}
-                              className={activeTab === 'Members' ? styles.active : ''}>Members</ListItem>
-                </UnorderedList>
-            </Flex>
         </>
     )
 }
