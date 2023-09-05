6ad501d2e78dae872e765be31a5b5ed26e3bcd02
Andrew Samboy <samboyandrew@gmail.com>
2023-09-05 08:17:39 -0500

    inbox header, click on project button to go to project page

--- src/components/project/inbox-header-projects-list.tsx
+++ src/components/project/inbox-header-projects-list.tsx
@@ -33,15 +33,16 @@ export function InboxHeaderProjectsList() {
         <>
             <Flex padding={'16px 40px'} backgroundColor={'#FFFFFF'} borderBottom={'1px solid #F3F4F6'} gap={3}
                   overflowX={'auto'}>
-                {projectData && !!projectData.length && (projectData || []).map((item: Project, index: number) => (
-                    <Flex align={'center'} key={index} gap={2} backgroundColor={'#FFFFFF'} border={'1px solid #F3F4F6'}
+                {projectData && !!projectData.length && (projectData || []).map((project: Project, index: number) => (
+                    <Flex onClick={() => Router.push(`/projects/${project.id}`)}
+                          align={'center'} key={index} gap={2} backgroundColor={'#FFFFFF'} border={'1px solid #F3F4F6'}
                           borderRadius={'8px'} padding={'8px'} minWidth={'216px'} maxWidth={'216px'}>
                         <Flex className={'disney-icon'} position={'relative'} align={'center'} justify={'center'}
                               borderRadius={'3px'} backgroundColor={'#C5D5ED'} h={'20px'} w={'20px'}>
                             <DisneyDIcon/>
                         </Flex>
                         <Text whiteSpace={'nowrap'} overflow={'hidden'} textOverflow={'ellipsis'} fontSize='13px'
-                              color={'#0A101D'} flex={'1'}>{item.name}</Text>
+                              color={'#0A101D'} flex={'1'}>{project.name}</Text>
                         <Flex className={'member-images'}>
                             <div className={'member-photo'}>
                                 <Image src="/image/user.png" width="24" height="24" alt=""/>
