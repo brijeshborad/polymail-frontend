b5874ce6361288a82d5ea4fdaa5d83fcb7bdb4e1
Brijesh Borad <brijeshborad29@gmail.com>
2023-09-08 11:32:15 +0530

    Height for sub headers

--- src/components/project/inbox-header-projects-list.tsx
+++ src/components/project/inbox-header-projects-list.tsx
@@ -25,20 +25,20 @@ export function InboxHeaderProjectsList() {
 
     return (
         <>
-            <Flex padding={'16px 40px'} backgroundColor={'#FFFFFF'} borderBottom={'1px solid #F3F4F6'} gap={3}
+            <Flex padding={'16px 40px 15px'} backgroundColor={'#FFFFFF'} borderBottom={'1px solid #F3F4F6'} gap={3}
                   overflowX={'auto'}>
                 {projectData && !!projectData.length && (projectData || []).map((project: Project, index: number) => (
                     <Button onClick={() => Router.push(`/projects/${project.id}`)}
                             key={index} gap={2} textAlign={'left'} backgroundColor={'#FFFFFF'}
-                            border={'1px solid #F3F4F6'}
-                            borderRadius={'8px'} padding={'8px'} minWidth={'216px'} maxWidth={'216px'}>
+                            border={'1px solid #F3F4F6'} h={'fit-content'}
+                            borderRadius={'8px'} padding={'7px'} minWidth={'216px'} maxWidth={'216px'}>
                         <Flex className={'disney-icon'} position={'relative'} align={'center'} justify={'center'}
                               borderRadius={'3px'} backgroundColor={'#C5D5ED'} h={'20px'} w={'20px'}>
                             <DisneyDIcon/>
                         </Flex>
                         <Text whiteSpace={'nowrap'} overflow={'hidden'} textOverflow={'ellipsis'} fontSize='13px'
                               color={'#0A101D'} flex={'1'}>{project.name}</Text>
-                        <Flex className={'member-images'}>
+                        <Flex className={'member-images subheader-images'}>
                             <div className={'member-photo'}>
                                 <Image src="/image/user.png" width="24" height="24" alt=""/>
                             </div>
@@ -49,8 +49,8 @@ export function InboxHeaderProjectsList() {
                 }
 
                 <Button alignItems={'center'} gap={2} textAlign={'left'} backgroundColor={'#FFFFFF'}
-                        onClick={() => changePage()}
-                        border={'1px solid #F3F4F6'} borderRadius={'8px'} padding={'8px'} minWidth={'216px'}
+                        onClick={() => changePage()} padding={'7px'} minWidth={'216px'}
+                        border={'1px solid #F3F4F6'} borderRadius={'8px'} h={'fit-content'}
                         maxWidth={'216px'}>
                     <div className={'folder-icon'}>
                         <FolderIcon/>
