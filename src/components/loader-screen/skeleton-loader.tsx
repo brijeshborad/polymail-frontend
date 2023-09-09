a9315e2c1165ad17fc839ed41958b9672ac42f7e
Luiz Eduardo Figueiredo <lerfbr@gmail.com>
2023-09-09 16:01:22 -0300

    Refactor skeleton loader to match Figma's color

--- src/components/loader-screen/skeleton-loader.tsx
+++ src/components/loader-screen/skeleton-loader.tsx
@@ -11,6 +11,7 @@ export function SkeletonLoader(props: Loader) {
                 <Skeleton
                     key={index}
                     className={styles.mailListSkeleton}
+                    startColor='#F3F4F6' endColor='#f3f3f3'
                     height={props.height || '60px'}
                     borderRadius={'8px'}
                     border={'1px solid #E5E7EB'}
