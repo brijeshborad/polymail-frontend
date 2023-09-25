3c45cc92d5adee6f6a6d8acf86f449a4c1e4a1e8
Brijesh Borad <brijeshborad29@gmail.com>
2023-09-25 20:34:55 +0530

    Fix batch update issue

--- src/components/threads/selected-threads.tsx
+++ src/components/threads/selected-threads.tsx
@@ -1,6 +1,6 @@
 import styles from "@/styles/Inbox.module.css";
 import { StateType } from "@/types";
-import {Box, Button, Image, Text, Tooltip} from "@chakra-ui/react";
+import {Box, Button, createStandaloneToast, Image, Text, Tooltip} from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 const AddToProjectButton = dynamic(() => import("@/components/common").then(mod => mod.AddToProjectButton));
 import { ArchiveIcon, TimeSnoozeIcon, TrashIcon } from "@/icons";
@@ -8,36 +8,65 @@ import dynamic from "next/dynamic";
 import {batchUpdateThreads, updateThreadState} from "@/redux/threads/action-reducer";
 import { Toaster } from "../common/toaster";
 import { MAILBOX_ARCHIVE, MAILBOX_SNOOZED, MAILBOX_TRASH } from "@/utils/constants";
-import React from "react";
+import React, {useState} from "react";
 import {Thread} from "@/models";
 
 export default function SelectedThreads() {
   const { multiSelection: selectedThreadIds, threads } = useSelector((state: StateType) => state.threads)
   const dispatch = useDispatch();
   const messagePlural = (selectedThreadIds || []).length === 1 ? 'message' : 'messages'
-  
-  const notification = (title: string) => {
+  const [mailBoxName, setMailBoxName] = useState<string>('');
+  const { toast } = createStandaloneToast()
+
+  const notification = (title: string, thread: Thread = {}) => {
+    let polyToast = `poly-toast-${new Date().getTime().toString()}`;
     Toaster({
-      type: 'success',
+      type: thread.hasOwnProperty('mailboxes') ? 'undo_changes': 'success',
       title,
-      desc: `Your message has been scheduled`
+      desc: `Your message has been scheduled`,
+      id: polyToast,
+      ...(thread.hasOwnProperty('mailboxes') ? {
+        undoUpdateRecordClick: () => {
+          dispatch(updateThreadState({
+            selectedThread: null,
+          }))
+          let body = {
+            threadIds: selectedThreadIds || [],
+            mailboxes: thread.mailboxes || []
+          }
+          dispatch(batchUpdateThreads(body))
+          notification('Thread was moved from ' + mailBoxName.toLowerCase() + '.');
+          dispatch(updateThreadState({
+            isThreadSearched: false,
+            multiSelection: [],
+            threads: threads,
+            selectedThread: thread,
+            success: true,
+            updateSuccess: true
+          }))
+          toast.close(`${polyToast}`);
+        }
+      }: {})
     })
   }
 
   const moveThreadToMailBoxes = (type: string) => {
+    setMailBoxName(type);
     if(selectedThreadIds && selectedThreadIds.length) {
       let body = {
         threadIds: selectedThreadIds,
         mailboxes: [type]
       }
       dispatch(batchUpdateThreads(body))
-      notification(`${selectedThreadIds.length} ${messagePlural} has been ${type.toLowerCase()}`)
+      let threadData = (threads || []).filter((item : Thread) => selectedThreadIds.includes(item.id!));
+
       let threadsData = (threads || []).filter((item: Thread) => !selectedThreadIds.includes(item.id!));
+      notification(`${selectedThreadIds.length} ${messagePlural} has been ${type.toLowerCase()}`, threadData[0])
       dispatch(updateThreadState({
         threads: threadsData,
         selectedThread: threadsData[0],
         isThreadSearched: false,
-        multiSelection: []
+        multiSelection: [],
       }))
     }
   }
