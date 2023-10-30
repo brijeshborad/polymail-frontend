a6507a601742fd386e38647e7b5473bbbffd222c
Brijesh Borad <brijeshborad29@gmail.com>
2023-10-30 19:01:26 +0530

    Show expanded time tooltip for short time.

--- src/components/common/time.tsx
+++ src/components/common/time.tsx
@@ -2,6 +2,7 @@ import {Text} from "@chakra-ui/react";
 import {TimeProps} from "@/types";
 import {useEffect, useState} from "react";
 import dayjs from "dayjs";
+import Tooltip from "@/components/common/Tooltip";
 
 export function Time(props: TimeProps) {
     const [_elapsedTime, setElapsedTime] = useState(0);
@@ -28,6 +29,7 @@ export function Time(props: TimeProps) {
         const currentDate = dayjs();
         let numberOfDaysBetweenTwoDates: number = currentDate.diff(infoDate, 'day');
         let timeString: string = '';
+        let fullTime: string = '';
         if (props.isShowFullTime) {
             return dayjs(props.time).format('MM/DD/YYYY hh:mm A');
         }
@@ -44,13 +46,16 @@ export function Time(props: TimeProps) {
             } else {
                 timeString = `${numberOfHoursBetweenTwoDates} ${props.showTimeInShortForm ? 'h' : 'hours ago'}`;
             }
+            fullTime = 'Today at ' + infoDate.format('hh:mm:ss A');
         } else if (currentDate.format('YYYY') === infoDate.format('YYYY')) {
             timeString = dayjs(props.time).format('MMM DD');
+            fullTime = infoDate.format('DD/MM/YYYY') + ' at ' + infoDate.format('hh:mm:ss A');
         } else {
             timeString = dayjs(props.time).format('MM/DD/YYYY')
+            fullTime = infoDate.format('DD/MM/YYYY') + ' at ' + infoDate.format('hh:mm:ss A');
         }
 
-        return timeString;
+        return <Tooltip label={fullTime} placement={'top'}>{timeString}</Tooltip>;
     };
 
     return (
