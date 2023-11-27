3ce4c96e0e4cf2f6ac6adf34645873e91d5697d3
Brijesh Borad <brijeshborad29@gmail.com>
2023-11-27 15:47:12 +0530

    Revers spacing like before

--- src/pages/inbox/index.tsx
+++ src/pages/inbox/index.tsx
@@ -21,7 +21,7 @@ function InboxPage() {
             </Flex>
 
             <div className={styles.mailBg}>
-                <Grid className={styles.mailGrid} templateColumns='calc(461px - 34px) auto' gap={9} height={'100%'}>
+                <Grid className={styles.mailGrid} templateColumns='30% auto' gap={4} height={'100%'}>
                     <GridItem w='100%' className={'threadSideBar'} id={'inbox-thread-side-bar'}>
                         <ThreadsSideBar/>
                     </GridItem>
