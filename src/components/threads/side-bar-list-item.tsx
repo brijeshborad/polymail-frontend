41d8367f41d254875f1ae63f921496f341adcd1b
Brijesh Borad <brijeshborad29@gmail.com>
2023-11-28 14:51:05 +0530

    attachment dropdown

--- src/components/threads/side-bar-list-item.tsx
+++ src/components/threads/side-bar-list-item.tsx
@@ -1,19 +1,15 @@
 import styles2 from "@/styles/common.module.css";
 import styles from "@/styles/Inbox.module.css";
-import {Box, Button, Flex, Menu, MenuButton, MenuItem, MenuList, Text} from "@chakra-ui/react";
+import {Box, Flex, Text} from "@chakra-ui/react";
 import {Time, UsersOnline} from "@/components/common";
 import {DotIcon} from "@/icons";
 import {StateType, ThreadListItemProps} from "@/types";
 import {useDispatch, useSelector} from "react-redux";
 import React, {useEffect, useState} from "react";
 import {MAILBOX_UNREAD} from "@/utils/constants";
-import {MessageAttachments} from "@/models";
 import {keyNavigationService} from "@/services";
 import Tooltip from "../common/Tooltip";
 import {AttachmentIcon} from "@chakra-ui/icons";
-import {clearDebounce, debounce} from "@/utils/common.functions";
-import {DefaultExtensionType, defaultStyles, FileIcon} from "react-file-icon";
-import {getAttachmentDownloadUrl} from "@/redux/messages/action-reducer";
 
 
 export function ThreadsSideBarListItem(props: ThreadListItemProps) {
@@ -45,88 +41,6 @@ export function ThreadsSideBarListItem(props: ThreadListItemProps) {
     useEffect(() => {
         setIsClicked((props.thread.mailboxes || []).includes(MAILBOX_UNREAD));
     }, [props.thread, updateSuccess, error])
-
-    function showExtensionImages(item: string | undefined) {
-        if (item) {
-            const parts = item.split('.');
-            if (parts.length > 1) {
-                const extension: string = parts[parts.length - 1];
-                return extension;
-            }
-        }
-        return 'pdf'
-    }
-
-    function attachmentsMenu() {
-        return <Menu isOpen={isAttachmentOpen}>
-            <Tooltip label='List attachments' placement='bottom' customeOpenHandelEvent
-                     isOpenEvent={isAttachmentToolTipOpen}>
-                <MenuButton
-                    className={`${styles.tabListAttachmentButton} emoji-dropdown`} minWidth={'1px'} padding={'2px 6px'}
-                    cursor={'pointer'}
-                    backgroundColor={'inherit !important'} height={'auto'} outline={"none"}
-                    _focusVisible={{boxShadow: 'none'}} _hover={{background: 'none'}} _active={{background: 'none'}}
-                    fontSize={'13px'} color={'#6B7280'} as={Button} mx={1}
-                    onClick={(e) => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        setIsAttachmentOpen(true)
-                        //close tooltip
-                        setIsAttachmentToolTipOpen(false);
-
-                    }}
-                    onMouseEnter={() => {
-                        setIsAttachmentToolTipOpen(true);
-                        if (isAttachmentOpen) {
-                            clearDebounce(props.thread.id + 'attachments');
-                        }
-                    }}
-                    onMouseLeave={() => {
-                        setIsAttachmentToolTipOpen(false);
-                        debounce(() => {
-                            setIsAttachmentOpen(false)
-                        }, 200, props.thread.id + 'attachments')
-                    }}
-                >
-                    <Flex alignItems={'center'}>
-                        <AttachmentIcon/>
-                    </Flex>
-                </MenuButton>
-            </Tooltip>
-            <MenuList
-                className={`${styles.tabListDropDown} drop-down-list`}
-                onMouseEnter={() => {
-                    if (isAttachmentOpen) {
-                        clearDebounce(props.thread.id + 'attachments');
-                    }
-                }}
-                onMouseLeave={() => {
-                    debounce(() => {
-                        setIsAttachmentOpen(false)
-                    }, 100, props.thread.id + 'attachments')
-                }}
-            >
-                {(props.thread.attachments || []).map((item: MessageAttachments, i: number) => (
-                    <MenuItem gap={2} key={i} className={'attachment-icon'} onClick={(e) => {
-                        e.preventDefault();
-                        e.stopPropagation();
-                        downloadImage(item)
-                    }}>
-                        <FileIcon
-                            extension={showExtensionImages(item.filename) as DefaultExtensionType}
-                            {...defaultStyles[showExtensionImages(item.filename) as DefaultExtensionType]}
-                        /> {item.filename}
-                    </MenuItem>
-                ))}
-
-            </MenuList>
-
-        </Menu>
-    }
-
-    const downloadImage = (item: MessageAttachments) => {
-        dispatch(getAttachmentDownloadUrl({body: {id: item.messageId, attachment: item.id}}));
-    }
 
     function buildSentNameAndTooltip() {
         let to: any = props?.thread?.latestSentMessage?.to || [];
@@ -210,7 +124,7 @@ export function ThreadsSideBarListItem(props: ThreadListItemProps) {
                         {isClicked &&
                         <DotIcon marginRight={'5px'} className={`readThreadIcon`}
                                  color={props.thread.snooze ? '#FF5E2C' : '#9ca3af'}/>}
-                        {props.thread.attachments && props.thread.attachments?.length > 0 && attachmentsMenu()}
+                        {props.thread.attachments && props.thread.attachments?.length > 0 && <AttachmentIcon/>}
                         <Time time={props.thread.latestMessage} isShowFullTime={false} showTimeInShortForm={false}/>
                     </Flex>
                 </Flex>
