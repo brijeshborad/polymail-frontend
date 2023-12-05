24601689199e0565d51e5bdb5ae0c786ee879a4c
Brijesh Borad <brijeshborad29@gmail.com>
2023-12-05 11:25:19 +0530

    automation create modal UI

--- src/components/project/upsert-project-rule.tsx
+++ src/components/project/upsert-project-rule.tsx
@@ -48,58 +48,50 @@ export function UpsertProjectRule({isOpen, onClose, type}: { isOpen: boolean, on
                 <Heading as='h3' fontSize={'13px'} fontWeight={'500'} letterSpacing={'-0.13px'} color={'#101828'}> Create Project Rule </Heading>
             </ModalHeader>
             <ModalCloseButton top={'15px'} right={'12px'} fontSize={'12px'} color={'#6B7280'} className={styles.closeIcon}/>
-            <ModalBody padding={'16px 12px'}>
+            <ModalBody padding={'16px 12px'} borderBottom={'1px solid #F3F4F6'}>
                 <Flex direction={'column'}>
-                    <Text fontSize='10px' color={'#6B7280'} fontWeight={'500'} lineHeight={'normal'} textTransform={'uppercase'} mb={1}>IF EMAIL IS FROM</Text>
-                    <Flex border={'1px solid #F3F4F6'} direction={'column'} borderRadius={'12px'} padding={3}>
+                    <Text fontSize='10px' color={'#6B7280'} fontWeight={'500'} lineHeight={'normal'} textTransform={'uppercase'} pl={3} mb={1}>IF EMAIL IS FROM</Text>
+                    <Flex border={'1px solid #F3F4F6'} direction={'column'} borderRadius={'12px'} padding={3} gap={6}>
                         <Flex gap={3}>
-                            <Flex className={'custom-radio-button'}>
+                            <Flex className={'custom-radio-button'} width={'50%'}>
                                 <Radio value='1'>Sender email</Radio>
                             </Flex>
 
-                            <Flex className={'custom-radio-button'}>
-                                <Radio value='1'>Sender domain</Radio>
+                            <Flex className={'custom-radio-button'} width={'50%'}>
+                                <Radio value='2'>Sender domain</Radio>
                             </Flex>
                         </Flex>
+
+                        <FormControl w={'100%'}>
+                            <FormLabel color={'#374151'} fontSize={'13px'} lineHeight={'1'} letterSpacing={'-0.13px'} mb={2}>Email Address</FormLabel>
+                            <Input border={'1px solid #E5E7EB'} borderRadius={8} color={'#0A101D'} fontSize={'14px'} h={'36px'} placeholder='Enter email address' value={projectRuleValues.value}
+                                   onChange={(e) => setProjectRuleValues(prevState => ({
+                                       ...prevState, value: e.target.value
+                                   }))}/>
+                        </FormControl>
                     </Flex>
                 </Flex>
-                <Flex alignItems={'flex-start'} direction={'column'} gap={4}>
-                    <FormControl w={'100%'}>
-                        <FormLabel>Select project</FormLabel>
-                        <Select variant='outline' placeholder='Select Project' value={projectRuleValues.projectId}
-                                onChange={(value) => setProjectRuleValues(prevState => ({
-                                    ...prevState, projectId: value.target.value
-                                }))}>
-                            {projects && projects.map((item: Project) => (
-                                <option key={item.id} value={item.id}>{item.emoji} {item.name}</option>
-                            ))}
-                        </Select>
-                    </FormControl>
-                    <FormControl w={'100%'}>
-                        <FormLabel>Select filter type</FormLabel>
-                        <RadioGroup onChange={(value) => setProjectRuleValues(prevState => ({
-                            ...prevState, filterType: value as any
-                        }))} value={projectRuleValues.filterType}>
-                            <Stack direction='row'>
-                                <Radio value='domain'>Domain</Radio>
-                                <Radio value='email'>Email</Radio>
-                            </Stack>
-                        </RadioGroup>
-                    </FormControl>
-                    <FormControl w={'100%'}>
-                        <FormLabel>Enter filter value</FormLabel>
-                        <Input placeholder='Enter filter value' value={projectRuleValues.value}
-                               onChange={(e) => setProjectRuleValues(prevState => ({
-                                   ...prevState, value: e.target.value
-                               }))}/>
-                    </FormControl>
+                <Flex direction={'column'} mt={12}>
+                    <Text fontSize='10px' color={'#6B7280'} fontWeight={'500'} lineHeight={'normal'} textTransform={'uppercase'} pl={3} mb={1}>ADD IT TO</Text>
+                    <Flex border={'1px solid #F3F4F6'} direction={'column'} borderRadius={'12px'} padding={3} gap={6}>
+                        <FormControl w={'100%'}>
+                            <FormLabel color={'#374151'} fontSize={'13px'} lineHeight={'1'} letterSpacing={'-0.13px'} mb={2}>Project</FormLabel>
+                            <Select border={'1px solid #E5E7EB'} borderRadius={8} color={'#0A101D'} fontSize={'14px'} h={'36px'} variant='outline' placeholder='Select Project' value={projectRuleValues.projectId}
+                                    onChange={(value) => setProjectRuleValues(prevState => ({
+                                        ...prevState, projectId: value.target.value
+                                    }))}>
+                                {projects && projects.map((item: Project) => (
+                                    <option key={item.id} value={item.id}>{item.emoji} {item.name}</option>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Flex>
                 </Flex>
             </ModalBody>
 
-            <ModalFooter className={styles.settingButton} paddingBottom={'40px'}>
-                <Button className={styles.settingCancel} colorScheme='blue' mr={3}
-                        onClick={onClose}> Cancel </Button>
-                <Button className={styles.settingSave} onClick={() => submit()} variant='ghost'>Add</Button>
+            <ModalFooter padding={'16px 12px 12px'}>
+                <Button borderRadius={'8px'} height={'auto'} fontSize={'14px'} padding={'9px 12px'} mr={3} border={'1px solid #1F2937'} backgroundColor={'#FFF'} color={'#1F2937'} _hover={{backgroundColor: '#1F2937', color: '#FFF'}} onClick={onClose}> Cancel </Button>
+                <Button borderRadius={'8px'} height={'auto'} fontSize={'14px'} padding={'9px 12px'} mr={3} border={'1px solid #1F2937'} backgroundColor={'#1F2937'} color={'#FFFFFF'} _hover={{backgroundColor: '#FFFFFF', color: '#1F2937'}} onClick={() => submit()} variant='ghost'>Create Rule</Button>
             </ModalFooter>
         </ModalContent>
     </Modal>
