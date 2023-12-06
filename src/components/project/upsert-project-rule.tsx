799f96e9fba41d2eb89f500b4585f9b23572b531
Brijesh Borad <brijeshborad29@gmail.com>
2023-12-06 11:46:50 +0530

    Automation select menu changes

--- src/components/project/upsert-project-rule.tsx
+++ src/components/project/upsert-project-rule.tsx
@@ -40,7 +40,6 @@ export function UpsertProjectRule({
     }, [type, editValue])
 
     function submit() {
-        let project = [...(projects || [])].find((t: Project) => t.id === projectRuleValues.projectId);
         let body = {
             projectId: projectRuleValues.projectId,
             accountId: selectedAccount?.id,
@@ -49,7 +48,7 @@ export function UpsertProjectRule({
         };
         let toaster = {
             desc: `Rule ${type === 'create' ? 'Created' : 'Updated'}`,
-            title: `Rule for ${project?.name} ${project?.emoji} has been ${type === 'create' ? 'created' : 'updated'}`,
+            title: `Rule for ${projectRuleValues.project?.name} ${projectRuleValues.project?.emoji} has been ${type === 'create' ? 'created' : 'updated'}`,
             type: 'success'
         };
         let finalBody: any = {
@@ -114,30 +113,24 @@ export function UpsertProjectRule({
                         <FormControl w={'100%'}>
                             <FormLabel color={'#374151'} fontSize={'13px'} lineHeight={'1'} letterSpacing={'-0.13px'}
                                        mb={2}>Project</FormLabel>
-
                             <Menu isLazy matchWidth={true}>
                                 <MenuButton textAlign={'left'} border={'1px solid #E5E7EB'} backgroundColor={'#FFF'}
                                             borderRadius={'8px'} fontSize={'13px'} lineHeight={1} padding={'10px 12px'}
-                                            height={'fit-content'} width={'100%'} as={Button} className={styles.selectProjectButton}
+                                            height={'fit-content'} width={'100%'} as={Button}
+                                            className={styles.selectProjectButton}
                                             rightIcon={<TriangleDownIcon/>}>
-                                    Select Project
+                                    {projectRuleValues.project ? `${projectRuleValues.project.emoji} ${projectRuleValues.project.name}` : 'Select Project'}
                                 </MenuButton>
                                 <MenuList className={'drop-down-list'}>
                                     {projects && projects.map((item: Project) => (
-                                        <MenuItem key={item.id}>{item.emoji} {item.name}</MenuItem>
+                                        <MenuItem key={item.id} onClick={() => {
+                                            setProjectRuleValues(prevState => ({
+                                                ...prevState, projectId: item.id, project: item
+                                            }))
+                                        }}>{item.emoji} {item.name}</MenuItem>
                                     ))}
                                 </MenuList>
                             </Menu>
-                            {/*<Select border={'1px solid #E5E7EB'} borderRadius={8} color={'#0A101D'} fontSize={'14px'}*/}
-                            {/*        h={'36px'} variant='outline' placeholder='Select Project'*/}
-                            {/*        value={projectRuleValues.projectId}*/}
-                            {/*        onChange={(value) => setProjectRuleValues(prevState => ({*/}
-                            {/*            ...prevState, projectId: value.target.value*/}
-                            {/*        }))}>*/}
-                            {/*    {projects && projects.map((item: Project) => (*/}
-                            {/*        <MenuItem key={item.id} value={item.id}>{item.emoji} {item.name}</MenuItem>*/}
-                            {/*    ))}*/}
-                            {/*</Select>*/}
                         </FormControl>
                     </Flex>
                 </Flex>
