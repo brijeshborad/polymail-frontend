fd927222cd9d07b2b983c42c0c18d8aaa74ae307
Andrew Samboy <samboyandrew@gmail.com>
2023-12-09 14:27:43 -0600

    senders email domain

--- src/components/common/add-to-project-button.tsx
+++ src/components/common/add-to-project-button.tsx
@@ -265,7 +265,7 @@ export function AddToProjectButton({
                         emails to this project?</Heading>
                     <MenuItem className={styles.newEmailButton}
                               onClick={() => addAutomationRulForProject('domain', `@${selectedThread?.from?.email.split('@')[1]}`)}>
-                        If sender's email domain is <Link>{`@${selectedThread?.from?.email.split('@')[1]}`}</Link>
+                        If sender&apos;s email domain is <Link>{`@${selectedThread?.from?.email.split('@')[1]}`}</Link>
                     </MenuItem>
                     <MenuItem className={styles.newEmailButton}
                               onClick={() => addAutomationRulForProject('email', selectedThread?.from?.email || '')}>
