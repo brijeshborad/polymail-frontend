3a13cca512228290db8efbd0c438ab767d32645c
Brijesh Borad <brijeshborad29@gmail.com>
2023-12-11 14:49:38 +0530

    Tooltip for inviting members, Dashed line spacing, Members hover icon not turning black

--- src/icons/members.icon.tsx
+++ src/icons/members.icon.tsx
@@ -5,16 +5,16 @@ export function MembersIcon() {
         <Icon viewBox="0 0 20 20" color='currentColor' fill="none">
             <path
                 d="M4.81807 16.0415H11.0153C11.4857 16.0415 11.8558 15.6513 11.7621 15.1903C11.5033 13.9175 10.6581 11.6665 7.91667 11.6665C5.17526 11.6665 4.33005 13.9175 4.07125 15.1903C3.97751 15.6513 4.34766 16.0415 4.81807 16.0415Z"
-                stroke="#9CA3AF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
             <path
                 d="M13.125 11.6665C14.8573 11.6665 15.5668 13.4564 15.8532 14.7465C16.0079 15.4432 15.4444 16.0415 14.7308 16.0415H13.9583"
-                stroke="#9CA3AF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
             <path
                 d="M7.91667 8.54183C9.18232 8.54183 10.2083 7.51582 10.2083 6.25016C10.2083 4.98451 9.18232 3.9585 7.91667 3.9585C6.65101 3.9585 5.625 4.98451 5.625 6.25016C5.625 7.51582 6.65101 8.54183 7.91667 8.54183Z"
-                stroke="#9CA3AF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
             <path
                 d="M12.2917 8.54183C13.5574 8.54183 14.375 7.51581 14.375 6.25016C14.375 4.98451 13.5574 3.9585 12.2917 3.9585"
-                stroke="#9CA3AF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
         </Icon>
     )
 }
